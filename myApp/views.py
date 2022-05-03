7e220a2bf32ae5760c02011b0eb515e830a68a12
ankur <ankur031100@gmail.com>
2022-05-03 17:16:09 +0530

    first commit

---
+++ myApp/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Create your views here.
+
+path ='myApp\chromedriver.exe'
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(path, options=options)
+driver.maximize_window()
+
+def home(request):
+    
+    driver.get('https://www.google.com')
+    title = driver.title
+    return render(request, 'index.html',{'title':title})
